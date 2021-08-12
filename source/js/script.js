9ec2a90782076cc01021e457a8b05b81a033a248
Alexander <alexanderkiryushkin@yandex.ru>
2021-08-13 01:04:44 +0300

    осталось валидацию и заголовок

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,26 @@
+const body = document.querySelector('.page__body');
 const header = document.querySelector('.page-header');
 const toggleNav = document.querySelector('.page-header__toggle');
+const mainNavClose = document.querySelector('.main-nav__list');
+const menuItem = document.querySelector('.main-nav__link')
 
 header.classList.remove('page-header--nojs');
 
 toggleNav.addEventListener ('click', () => {
   if (header.classList.contains('page-header--opened')) {
     header.classList.remove('page-header--opened');
+    body.style.overflow = 'scroll';
   } else {
     header.classList.add('page-header--opened');
+    body.style.overflow = 'hidden';
+  }
+});
+
+mainNavClose.addEventListener ('click', () => {
+  if (menuItem.classList.contains('main-nav__link')) {
+    header.classList.remove('page-header--opened');
+    body.style.overflow = 'scroll';
+  } else {
+    return
   }
 });
