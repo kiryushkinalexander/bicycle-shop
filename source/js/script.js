0d83e094968bc9f344ac5be65fb4745237d1240f
Alexander <alexanderkiryushkin@yandex.ru>
2021-08-17 23:57:53 +0300

    исправил overflow + футер на гридах

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('.page__body');
 const header = document.querySelector('.page-header');
 const toggleNav = document.querySelector('.page-header__toggle');
+const nav = document.querySelector('.page-header__nav')
 const mainNavClose = document.querySelector('.main-nav__list');
 const menuItem = document.querySelector('.main-nav__link');
 const anchors = document.querySelectorAll('a[href*="#"]');
@@ -30,19 +31,18 @@ header.classList.remove('page-header--nojs');
 toggleNav.addEventListener ('click', () => {
   if (header.classList.contains('page-header--opened')) {
     header.classList.remove('page-header--opened');
-    body.style.overflow = 'scroll';
+    body.style.overflowY = 'scroll';
   } else {
     header.classList.add('page-header--opened');
-    body.style.overflow = 'hidden';
+    body.style.overflowY = 'hidden';
+    nav.style.overflowY = 'auto';
   }
 });
 
 mainNavClose.addEventListener ('click', () => {
   if (menuItem.classList.contains('main-nav__link')) {
     header.classList.remove('page-header--opened');
-    body.style.overflow = 'scroll';
-  } else {
-    return
+    body.style.overflowY = 'scroll';
   }
 });
 
